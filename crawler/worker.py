ba8984be7a96f709028e22e95f3b4c7b82dffcb7
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-01 18:00:57 +0900

    import Models, method from database

--- crawler/worker.py
+++ crawler/worker.py
@@ -3,7 +3,7 @@
 from celery import Celery
 from datetime import timedelta
 from .config import CELERY_BROKER_URI
-from .database import instant_session
+from .database import instant_session, Cafe, CafeBoard, CafeArticle, get_or_create
 
 instance = Celery('crawler',
                   broker=CELERY_BROKER_URI)
@@ -33,5 +33,3 @@ def save_article():
     session = instant_session()
     # Act as db session
     session.remove()
-
-
